91a71c52c67408b18978f290aea8b6d7da28b807
Yair Raviv <yraviv@nvidia.com>
2026-01-25 11:18:54 +0200

    Fast reboot | Added multi-ASIC support
    Signed-off-by: Yair Raviv <yraviv@nvidia.com>

--- tests/fast_reboot_filter_routes_test.py
+++ tests/fast_reboot_filter_routes_test.py
@@ -11,7 +11,7 @@ class TestFastRebootFilterRoutes(object):
     @patch('utilities_common.cli.run_command')
     def test_get_connected_routes(self, mock_run_command):
         mock_run_command.return_value = ('{"1.1.0.0/16": {}}', 0)
-        output = fast_reboot_filter_routes.get_connected_routes()
+        output = fast_reboot_filter_routes.get_connected_routes(namespace="")
         mock_run_command.assert_called_with(['sudo', 'vtysh', '-c', "show ip route connected json"], return_cmd=True)
         assert output == ['1.1.0.0/16']
 
@@ -19,7 +19,7 @@ class TestFastRebootFilterRoutes(object):
     def test_get_connected_routes_command_failed(self, mock_run_command):
         mock_run_command.return_value = ('{"1.1.0.0/16": {}}', 1)
         with pytest.raises(Exception):
-            fast_reboot_filter_routes.get_connected_routes()
+            fast_reboot_filter_routes.get_connected_routes(namespace="")
         mock_run_command.assert_called_with(['sudo', 'vtysh', '-c', "show ip route connected json"], return_cmd=True)
 
     def teardown_method(self):
